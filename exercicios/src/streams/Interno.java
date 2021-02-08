17e94240cd174f6d23f97fc2e8ec611d0b58e852
GeiBatista <geibatista@hotmail.com>
2021-02-07 22:00:41 -0300

    Filter

---
+++ exercicios/src/streams/Interno.java
@@ -0,0 +1,15 @@
+package streams;
+
+public class Interno {
+	
+	final String nome;
+	final int idade;
+	final String regime;
+	
+	public Interno(String nome, int idade, String regime) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.regime = regime;
+	}	
+}
