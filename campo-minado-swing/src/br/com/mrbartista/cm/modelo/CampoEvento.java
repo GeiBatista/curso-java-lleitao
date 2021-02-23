ed67a673533752422f5d2d0b4f784d2d1cf69706
GeiBatista <geibatista@hotmail.com>
2021-02-22 21:18:47 -0300

    campo minado com interface grafica swing

--- campo-minado-swing/src/br/com/mrbartista/cm/modelo/CampoEvento.java
+++ campo-minado-swing/src/br/com/mrbartista/cm/modelo/CampoEvento.java
@@ -2,6 +2,6 @@ package br.com.mrbartista.cm.modelo;
 
 public enum CampoEvento {
 	
-	ABRIR, MARCAR, DESMARCAR, EXPLODIR
+	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
 
 }
--- campo-minado-swing/src/br/com/mrbartista/cm/modelo/Tabuleiro.java
+++ campo-minado-swing/src/br/com/mrbartista/cm/modelo/Tabuleiro.java
@@ -7,9 +7,9 @@ import java.util.function.Predicate;
 
 public class Tabuleiro implements CampoObservador{
 
-	private int linhas;
-	private int colunas;
-	private int minas;
+	private final int linhas;
+	private final int colunas;
+	private final int minas;
 
 	private final List<Campo> campos = new ArrayList<>();
 	private List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
@@ -24,6 +24,18 @@ public class Tabuleiro implements CampoObservador{
 		sortearMinas();
 	}
 	
+	public void paraCadaCampo(Consumer<Campo> funcao) {
+		campos.forEach(funcao);
+	}
+		
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
 	public void registrarObservador(Consumer<ResultadoEvento> observador) {
 		observadores.add(observador);
 	}
@@ -84,6 +96,7 @@ public class Tabuleiro implements CampoObservador{
 	@Override
 	public void eventoOcorreu(Campo campo, CampoEvento evento) {
 		if(evento == CampoEvento.EXPLODIR) {
+			mostrarMinas();
 			notificarObservadores(false);
 		}else if(objetivoAlcancado()) {
 			notificarObservadores(true);
@@ -93,6 +106,7 @@ public class Tabuleiro implements CampoObservador{
 	private void mostrarMinas() {
 		campos.stream()
 		.filter(c -> c.isMinado())
+		.filter(c -> !c.isMarcado())
 		.forEach(c -> c.setAberto(true));
 		
 	}
